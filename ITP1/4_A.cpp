429f11fb0ca07bbfce356a521cd2208bcd6bb96c
ochi <ocos54ce@gmail.com>
2021-06-26 00:16:20 +0900

    solve ITP1_4_A

---
+++ ITP1/4_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a,b;cin >> a >> b;
+	int d = a / b;
+	int r = a % b;
+	double aa = a * 1.0;
+	double bb = b * 1.0;
+	double f = aa / bb;
+	printf("%d %d %lf\n", d, r, f);
+}
